8f41beff724c0cb5575b84d35887ca7a701bac04
ceto2016 <ceto2016@gmail.com>
2022-05-22 17:51:08 +0200

    add colors amd assets

--- lib/core/constant/constants.dart
+++ lib/core/constant/constants.dart
@@ -2,6 +2,39 @@ import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 
 class Constants {
+  static Color primaryColor = const Color(0xFF10519A);
+
+  static Color primaryDarkColor = const Color(0xFF0D2137);
+  static Color accentColor = const Color(0xFF00B4D8);
+
+  static List<Color> colorGrediant = const [
+    Color(0xFF1E7879),
+    Color(0xFF043E49)
+  ];
+  // font color
+  // buttons
+  static ButtonStyle buttonStyle = ButtonStyle(
+    backgroundColor: MaterialStateProperty.all(primaryColor),
+    shadowColor: MaterialStateProperty.all(Colors.transparent),
+    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+      RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(30.0),
+        side: const BorderSide(color: Colors.white),
+      ),
+    ),
+  );
+  static ButtonStyle getbuttonStyleRounded(Color color) {
+    return ButtonStyle(
+      backgroundColor: MaterialStateProperty.all(color),
+      shadowColor: MaterialStateProperty.all(Colors.transparent),
+      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+        RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(16.0),
+        ),
+      ),
+    );
+  }
+
   static double width(context) {
     return MediaQuery.of(context).size.width;
   }
@@ -34,10 +67,7 @@ class Constants {
     );
   }
 
-  static showToast({
-    required String message,
-    required Color color,
-  }) {
+  static showToast({required String message, required Color color}) {
     return Fluttertoast.showToast(
         msg: message,
         toastLength: Toast.LENGTH_SHORT,
